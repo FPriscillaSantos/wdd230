da585d5e8d905bf6f6b2ffc59bc9da5ced1dc613
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-06-21 00:06:24 -0300

    3

--- week2/scripts/bom.js
+++ week2/scripts/bom.js
@@ -1,3 +1,24 @@
-const input = document.querySelector('#favchap');
+const list = document.querySelector('#ul');
+const input = document.querySelector('input');
 const button = document.querySelector('button');
-const list = document.querySelector('#list');
+
+button.addEventListener('click'), () => {
+    const myChapter = input.value;
+    input.value = '';
+
+    const listChapter = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listChapter.appendChild(listText);
+    listText.textContent = myChapter;
+    listChapter.appendChild(listBtn);
+    listBtn.textContent = 'X';
+    list.appendChild(listChapter);
+
+    listBtn.addEventListener('click', () => {
+        list.removeChild(listChapter);
+    })
+
+    input.focus();
+}
