a6245f255a3f4fcea3191f1d933b64fce7616637
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-06-21 23:24:23 -0300

    dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
-const hamburgerElement = document.querySelector('#myButton');
-const navElement = document.querySelector('.menuLinks');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const hamburgerElement = document.querySelector('#myButton');
+    const navElement = document.querySelector('.menuLinks');
 
-hamburgerElement.addEventListener('click', () => {
-    navElement.classList.toggle('open');
-    hamburgerElement.classList.toggle('open');
+    hamburgerElement.addEventListener('click', () => {
+        navElement.classList.toggle('open');
+        hamburgerElement.classList.toggle('open');
+    });
+
+    const modeButton = document.querySelector("#mode");
+    const body = document.body;
+
+    modeButton.addEventListener("click", () => {
+        body.classList.toggle("dark-mode");
+    });
 });
 
