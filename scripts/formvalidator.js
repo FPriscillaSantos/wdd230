b44090e54107136131c41934ab7829bd1d0762df
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-07-08 01:01:10 -0300

    falta o email

--- scripts/formvalidator.js
+++ scripts/formvalidator.js
@@ -1,10 +1,26 @@
-// Seleciona os elementos do range e do display do valor
 const range = document.getElementById('r');
 const rangevalue = document.getElementById('rangevalue');
 
-// Adiciona event listeners para atualizar o valor do range em tempo real
 range.addEventListener('input', function() {
     rangevalue.textContent = this.value; // Atualiza o valor do range
 });
 
+const w1 = document.querySelector("#password1");
+const w2 = document.querySelector("#password2");
+const fb = document.querySelector("#feedback")
 
+w2.addEventListener('focusout', controlar)
+
+function controlar() {
+    //console.log('inside the function')
+    if (w1.value !== w2.value) {
+        //console.log('no match')
+        w1.value=""
+        w2.value=""
+        w1.focus()
+        fb.textContent= "Password values ​​do not match"
+    } else {
+        //console.log("match!")
+        fb.textContent=""
+    }
+}
