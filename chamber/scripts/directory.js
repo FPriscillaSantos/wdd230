5ba1b40fda89140437d653ee246f0dd4955aea18
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-07-15 21:38:45 -0300

    1

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -5,19 +5,36 @@ async function getMembersList() {
     const response = await fetch(url);
     const data = await response.json();
     console.table(data.members);
-    displayProphets(data.members);
+    displayMembers(data.members);
 }
-
-getMembersList();
 
 const displayMembers = (members) => {
     members.forEach((member) => {
         let card = document.createElement('section');
         let image = document.createElement('img');
-        let adrees = document.createElement('p');
-        let tel = document.createElement('p'); 
-        let url = document.createElement('p'); 
+        let name = document.createElement('h3');
+        let address = document.createElement('p');
+        let tel = document.createElement('p');
+        let url = document.createElement('a');
 
+        image.setAttribute('src', member.imageIcon || 'default-image.png'); // Use a default image if none is provided
+        image.setAttribute('alt', member.member);
+        name.textContent = member.member;
+        address.textContent = member.address;
+        tel.textContent = member.tel;
+        url.setAttribute('href', `https://${member.url}`);
+        url.textContent = member.url;
 
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(tel);
+        card.appendChild(url);
+
+        membersList.appendChild(card);
     });
 }
+
+getMembersList();
+
+
