fb2a84457f5c30363fcaa0e70afa394812ff47f2
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-07-16 01:14:02 -0300

    done

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,6 +1,9 @@
 const url = `data/members.json`;
 const membersList = document.querySelector('#membersList');
 
+// Adiciona a classe 'grid' ao carregar a página
+membersList.classList.add("grid");
+
 async function getMembersList() {
     const response = await fetch(url);
     const data = await response.json();
@@ -46,12 +49,19 @@ getMembersList();
 const gridButton = document.querySelector("#grid");
 const listButton = document.querySelector("#list");
 
+// Adiciona a classe 'selected' ao botão grid ao carregar a página
+gridButton.classList.add("selected");
+
 gridButton.addEventListener("click", () => {
     membersList.classList.add("grid");
     membersList.classList.remove("list");
+    gridButton.classList.add("selected");
+    listButton.classList.remove("selected");
 });
 
 listButton.addEventListener("click", () => {
     membersList.classList.add("list");
     membersList.classList.remove("grid");
+    listButton.classList.add("selected");
+    gridButton.classList.remove("selected");
 });
